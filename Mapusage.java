80c995f3b6925a84cc78223a18b9b851d6a0db68
Adnan Ali <shaikadnan1982@gmail.com>
2025-08-13 15:07:58 +0530

    fourth commit

--- Mapusage.java
+++ Mapusage.java
@@ -1,46 +1,47 @@
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Scanner;
 
 public class Mapusage{
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         Map<Integer , Integer> Maps=new  HashMap<>();
-        Maps.put(101,39);
-        Maps.put(102,98);
-        Maps.put(103,34);
-        Maps.put(104,56);
-        Maps.put(105,87);
-        Maps.put(106,87);
-        int rollno=102;
-        if(Maps.containsKey(rollno)){
-            System.out.println("marks of roll " + rollno + " : " + Maps.get(rollno));
-        }
-        else{
-            System.out.println("Student not found");
-        }
-        Maps.remove(106); 
+      System.out.println("Enter the Roll and Marks of the Studet: ");
+      for(Map.Entry<Integer, Integer> i : Maps.entrySet()){
+        int roll=sc.nextInt();
+        int Marks=sc.nextInt();
+        Maps.put(roll,Marks);
+      }
+        // int rollno=102;
+        // if(Maps.containsKey(rollno)){
+        //     System.out.println("marks of roll " + rollno + " : " + Maps.get(rollno));
+        // }
+        // else{
+        //     System.out.println("Student not found");
+        // }
+        // Maps.remove(106); 
         // int highestMarks=Integer.MIN_VALUE;
         // int highRoll=-1;
-        System.out.println("print all the student marks: ");
-        int countPass=0;
-        int countFail=0;
-        for(Map.Entry<Integer ,Integer> i : Maps.entrySet()){
-                //  System.out.println("Roll: " + i.getKey() + ", Marks : " + i.getValue());
-                // if(i.getValue() > highestMarks){
-                //    highestMarks=i.getValue();
-                //    highRoll=i.getKey();
+        // System.out.println("print all the student marks: ");
+        // int countPass=0;
+        // int countFail=0;
+    //     for(Map.Entry<Integer ,Integer> i : Maps.entrySet()){
+    //             //  System.out.println("Roll: " + i.getKey() + ", Marks : " + i.getValue());
+    //             // if(i.getValue() > highestMarks){
+    //             //    highestMarks=i.getValue();
+    //             //    highRoll=i.getKey();
                     
-                // }
-                if(i.getValue() >=40){
-                    countPass++;
-                }
-                else{
-                    countFail++;
-                }
+    //             // }
+    //             if(i.getValue() >=40){
+    //                 countPass++;
+    //             }
+    //             else{
+    //                 countFail++;
+    //             }
 
-        }
-        System.out.println("The  number of the students Pass  are :" + countPass +"\n The no.of Students Fail are : " + countFail);
+    //     }
+    //     System.out.println("The  number of the students Pass  are :" + countPass +"\n The no.of Students Fail are : " + countFail);
 
     }
 }
